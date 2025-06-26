43327c70e5e3a2adbb2a194bec1d5d7f63077da9
Guilherme Gomes <guilherme09396@gmail.com>
2025-06-25 22:22:49 -0300

    Criação do controller de reembolsos

---
+++ src/http/controllers/refunds/create-refunds.ts
@@ -0,0 +1,25 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { ResourceNotFoundError } from '@/services/errors/resource-not-found-error';
+import { makeCreateRefundsService } from '@/services/factories/make-create-refunds-service';
+import { NotFoundPaymentError } from '@/services/errors/not-found-payment-error';
+
+export async function createRefunds(req: FastifyRequest, res: FastifyReply) {
+  try {
+    const createRefundsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = createRefundsSchema.parse(req.params);
+
+    const createRefundsService = makeCreateRefundsService();
+    const { charge } = await createRefundsService.execute({ chargeId: id });
+    return res.status(201).send();
+  } catch (e) {
+    if (e instanceof ResourceNotFoundError || e instanceof NotFoundPaymentError) {
+      return res.status(403).send({ errors: e.message });
+    }
+
+    throw e;
+  }
+}
